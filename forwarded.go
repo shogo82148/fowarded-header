f915e0705abec618224fdac82e0c18d0f410738d
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-26 16:10:53 +0900

    implement Forwarded.String method

--- forwarded.go
+++ forwarded.go
@@ -1,14 +1,153 @@
 package fowardedheader
 
+import (
+	"io"
+	"strings"
+)
+
+// Forwarded represents the Forwarded header.
 type Forwarded struct {
-	By    string
-	For   string
-	Host  string
+	// By is used to disclose the interface where the request came in to the proxy server.
+	By string
+
+	// For is used to disclose information about the client that initiated the request and subsequent proxies in a chain of proxies.
+	For string
+
+	// Host is used to forward the original value of the "Host" header field.
+	Host string
+
+	// Proto is the value of the used protocol type.
 	Proto string
 }
 
+var validTchar = [256]bool{
+	'!':  true,
+	'#':  true,
+	'$':  true,
+	'%':  true,
+	'&':  true,
+	'\'': true,
+	'*':  true,
+	'+':  true,
+	'-':  true,
+	'.':  true,
+	'^':  true,
+	'_':  true,
+	'`':  true,
+	'|':  true,
+	'~':  true,
+
+	// DIGIT
+	'0': true,
+	'1': true,
+	'2': true,
+	'3': true,
+	'4': true,
+	'5': true,
+	'6': true,
+	'7': true,
+	'8': true,
+	'9': true,
+
+	// ALPHA
+	'a': true,
+	'b': true,
+	'c': true,
+	'd': true,
+	'e': true,
+	'f': true,
+	'g': true,
+	'h': true,
+	'i': true,
+	'j': true,
+	'k': true,
+	'l': true,
+	'm': true,
+	'n': true,
+	'o': true,
+	'p': true,
+	'q': true,
+	'r': true,
+	's': true,
+	't': true,
+	'u': true,
+	'v': true,
+	'w': true,
+	'x': true,
+	'y': true,
+	'z': true,
+	'A': true,
+	'B': true,
+	'C': true,
+	'D': true,
+	'E': true,
+	'F': true,
+	'G': true,
+	'H': true,
+	'I': true,
+	'J': true,
+	'K': true,
+	'L': true,
+	'M': true,
+	'N': true,
+	'O': true,
+	'P': true,
+	'Q': true,
+	'R': true,
+	'S': true,
+	'T': true,
+	'U': true,
+	'V': true,
+	'W': true,
+	'X': true,
+	'Y': true,
+	'Z': true,
+}
+
+func IsValidToken(s string) bool {
+	for _, r := range []byte(s) {
+		if !validTchar[r] {
+			return false
+		}
+	}
+	return true
+}
+
+func writeQuotedString(w io.ByteWriter, s string) {
+	w.WriteByte('"')
+	for _, r := range []byte(s) {
+		if r == '"' || r == '\\' {
+			w.WriteByte('\\')
+		}
+		w.WriteByte(r)
+	}
+	w.WriteByte('"')
+}
+
+func writePair(buf *strings.Builder, key, value string) {
+	if value == "" {
+		return
+	}
+	if buf.Len() > 0 {
+		buf.WriteByte(';')
+	}
+
+	buf.WriteString(key)
+	buf.WriteByte('=')
+	if IsValidToken(value) {
+		buf.WriteString(value)
+	} else {
+		writeQuotedString(buf, value)
+	}
+}
+
 func (f *Forwarded) String() string {
-	return ""
+	var buf strings.Builder
+	writePair(&buf, "by", f.By)
+	writePair(&buf, "for", f.For)
+	writePair(&buf, "host", f.Host)
+	writePair(&buf, "proto", f.Proto)
+	return buf.String()
 }
 
 func Parse(h []string) ([]*Forwarded, error) {
--- fowarded_test.go
+++ fowarded_test.go
@@ -5,6 +5,87 @@ import (
 	"testing"
 )
 
+func TestForwarded_String(t *testing.T) {
+	tests := []struct {
+		name string
+		f    *Forwarded
+		want string
+	}{
+		{
+			name: "empty",
+			f:    &Forwarded{},
+			want: "",
+		},
+		{
+			name: "for",
+			f: &Forwarded{
+				For: "_gazonk",
+			},
+			want: `for=_gazonk`,
+		},
+		{
+			name: "for-ipv6",
+			f: &Forwarded{
+				For: `[2001:db8:cafe::17]`,
+			},
+			want: `for="[2001:db8:cafe::17]"`,
+		},
+		{
+			name: "for-ipv4-and-port",
+			f: &Forwarded{
+				For: `192.0.2.43:47011`,
+			},
+			want: `for="192.0.2.43:47011"`,
+		},
+		{
+			name: "for-ipv6-and-port",
+			f: &Forwarded{
+				For: `[2001:db8:cafe::17]:4711`,
+			},
+			want: `for="[2001:db8:cafe::17]:4711"`,
+		},
+		{
+			name: "proto-http",
+			f: &Forwarded{
+				Proto: "http",
+			},
+			want: `proto=http`,
+		},
+		{
+			name: "proto-https",
+			f: &Forwarded{
+				Proto: "https",
+			},
+			want: `proto=https`,
+		},
+
+		{
+			name: "back-slash",
+			f: &Forwarded{
+				For: `\`,
+			},
+			want: `for="\\"`,
+		},
+		{
+			name: "double-quote",
+			f: &Forwarded{
+				For: `"`,
+			},
+			want: `for="\""`,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			f := tt.f
+			if got := f.String(); got != tt.want {
+				t.Errorf("Forwarded.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestParse(t *testing.T) {
 	tests := []struct {
 		name    string
